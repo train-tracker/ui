74828f1711fe7049cd6b2387e6992c4c119f7047
Robert Huffman <TheOnlyMerlin@gmail.com>
2015-01-23 20:47:29 -0800

    Fixed login again...

--- app/routes/controllers/login.js
+++ app/routes/controllers/login.js
@@ -1,9 +1,9 @@
 'use strict';
 
-module.exports = function($scope, User, $http) {
+module.exports = function($scope, User, $http, API_ROOT) {
     $scope.login = function (user) {
         // Handle login
-        $http.post(API_ROOT+'/login', data).success(function(){
+        $http.post(API_ROOT + '/login', user).success(function(){
             console.log(response)
         }).finally(function() {
             console.log("Finally!");
