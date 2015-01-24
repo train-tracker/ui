5840b345642d6d1e96a7067d9a51eef1bf7290a9
Robert Huffman <TheOnlyMerlin@gmail.com>
2015-01-24 00:09:55 -0800

    Added various routes for some of the new views.

--- app/routes/index.js
+++ app/routes/index.js
@@ -22,4 +22,46 @@ angular.module('routes', ['ui.router'])
         template: require('./views/login'),
         controller: 'login',
       })
+      // Users
+      .state('user', {
+        url: '/user',
+        template: require('./views/user'),
+        controller: 'user',
+      })
+      // Admins
+      .state('admin', {
+        url: '/admin',
+        template: require('./views/admin'),
+        controller: 'admin',
+      })
+      // Organizations
+      .state('orgs', {
+        url: '/orgs',
+        template: require('./views/orgs'),
+        controller: 'orgs',
+      })
+      // Modules
+      .state('modules', {
+        url: '/modules',
+        template: require('./views/modules'),
+        controller: 'modules',
+      })
+      // Classes
+      //.state('classes', {
+      //  url: '/classes',
+      //  template: require('./views/classes'),
+      //  controller: 'classes',
+      //})
+      // Courses
+      .state('courses', {
+        url: '/courses',
+        template: require('./views/courses'),
+        controller: 'courses',
+      })
+      // Static about page
+      .state('about', {
+        url: '/about',
+        template: require('./views/about'),
+      })
+      
   })
