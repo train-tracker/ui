362d4ea7bbb04b9f2c7825ce7530b1d8464a6bbb
Robert Huffman <TheOnlyMerlin@gmail.com>
2015-01-24 06:50:59 -0800

    Made inital version sof some controllers.

---
+++ app/routes/controllers/admin.js
@@ -0,0 +1,10 @@
+/* 
+ * See license.txt for license information.
+ */
+'use strict';
+
+admin.exports = function($scope, admin ) {
+    
+    $scope.admin = admin;
+    
+};
---
+++ app/routes/controllers/modules.js
@@ -0,0 +1,10 @@
+/* 
+ * See license.txt for license information.
+ */
+'use strict';
+
+module.exports = function($scope, modules ) {
+    
+    $scope.modules = modules;
+    
+};
