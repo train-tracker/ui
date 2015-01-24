0f5a863ef9903a6fe4de6b35277ec0bdb2130602
Robert Huffman <TheOnlyMerlin@gmail.com>
2015-01-24 00:10:32 -0800

    Added some placeholder content for handling users.

--- app/common/resources/user.js
+++ app/common/resources/user.js
@@ -1,4 +1,14 @@
 module.exports = function(Restangular) {
   var User = Restangular.all('user');
+  
+  User.resetPassword = function () {
+      
+  };
+  
+  User.verify = function() {
+      
+  };
+  
   return User;
 }
+
