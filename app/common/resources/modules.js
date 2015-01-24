e812fe20eb84e3966271b862db74b78164418f45
Robert Huffman <TheOnlyMerlin@gmail.com>
2015-01-24 03:02:58 -0800

    Create service for modules.  Doesn't look like it would be necessary, but if it's wanted, it's there.

---
+++ app/common/resources/modules.js
@@ -0,0 +1,12 @@
+/* 
+ * See license.txt for license information.
+ */
+
+
+module.exports = function(Restangular) {
+    var Module = Restangular.all('module');
+
+    
+    return Module;
+    
+}
