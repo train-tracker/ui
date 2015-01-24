cffd301f195b74f511949f9da8d58c069d8fbe88
Robert Huffman <TheOnlyMerlin@gmail.com>
2015-01-24 12:36:36 -0800

    'delayVideo' directive implemented.

--- app/common/directives/index.js
+++ app/common/directives/index.js
@@ -1,1 +1,19 @@
 angular.module('directives', [])
+  .directive('delayVideo', [function() {
+
+
+      function link(scope, element, attrs) {
+          
+          // When finished loading, update the element
+          $(element).onloadedmetadata = function() {
+                this.currentTime = scope.startTime;
+          };
+      }
+      
+      return {
+          link: link, 
+          scope: { height: "@",  width: "@", source: "@", startTime: "=" },
+          restrict: 'A',
+          template: require('./views/video')
+      };
+  }])
